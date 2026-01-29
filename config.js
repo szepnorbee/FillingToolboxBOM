2df467612b87ed736a55d75f8553be777b23a219
Szép Norbert <szepnorbee@gmail.com>
2026-01-29 18:14:43 +0100

    Add configuration for API and Google Sheet URLs

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+    // Ide másold be a Google Script Web App URL-t
+    API_URL: "https://script.google.com/macros/s/AKfycbw5JO6bvRVl6YaznLgIaw8URIL8okq-cH-1yj1ZuMdFzUXQ9e5TmGmRguJnechzk6GT8Q/exec",
+    
+    // Ide pedig a szerkeszthető Google Sheet linkjét
+    SHEET_URL: "https://docs.google.com/spreadsheets/d/1U6sbZG9ThOOW4blMD_HDU5SsiA6M2pNk7PY7rrSVVsU/edit?gid=1996609599#gid=1996609599"
+};
